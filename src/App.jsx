c067b4a3223ad8d97c8ff88fd7d06c29963c8747
= <pumaronal2016@gmail.com>
2024-06-10 12:00:15 -0400

    se modifico las 3 Fotografias resien

--- src/App.jsx
+++ src/App.jsx
@@ -11,11 +11,11 @@ import LayoutAdmin from "./layout/LayoutAdmin";
 import Inicio from "./pages/Inicio/Inicio";
 // import EducativaPublica from "./pages/UnidadesEducativas/Publico/EducativaPublica";
 // import EducativaPrivada from "./pages/UnidadesEducativas/Privada/EducativaPrivada";
-<<<<<<< HEAD
+
 // import CentroSalud from "./pages/CentrosSalud/CentroSalud";
-=======
+
 import CentroSalud from "./pages/CentrosSalud/CentroSalud";
->>>>>>> 89dd2e3cdb5e5ae54d807149a70d02891a8f2999
+
 import CentroRecreativo from "./pages/CentrosRecreativos/CentroRecreativo";
 import FormularioAgregarPublica from "./pages/UnidadesEducativas/Publico/FormularioAgregarPublica";
 import FormDataPublica from "./pages/UnidadesEducativas/Publico/FormDataPublica";
@@ -25,10 +25,9 @@ import ListaGeneralUE from "./pages/UnidadesEducativas/Publico/ListaGeneralUE";
 import FormAgregarUE from "./pages/UnidadesEducativas/FormAgregarNuevo/FormAgregarUE";
 import FormEditarUE from "./pages/UnidadesEducativas/FormEditar/FormEditarUE";
 import FormDataPublicaExtra from "./pages/UnidadesEducativas/Publico/FormDataPublicaExtra";
-<<<<<<< HEAD
+
 import CentroSaludGeneral from "./pages/CentrosSalud/CentroSaludGeneral";
-=======
->>>>>>> 89dd2e3cdb5e5ae54d807149a70d02891a8f2999
+
 // import UnidadEducativa from "./pages/UnidadesEducativas/UnidadEducativa";
 function App() {
  
@@ -36,19 +35,12 @@ function App() {
       <>
         <Router> {/* Envuelve todo dentro del componente Router */}
           <Routes>
-<<<<<<< HEAD
             
             <Route path="/inicio" element={<Principal/>}>
               <Route index element={<MainBienvenida/>} /> 
               <Route path="auth" element={<VentanaLogin/>} /> 
             </Route>
             
-=======
-            <Route path="/inicio" element={<Principal/>}>
-              <Route index element={<MainBienvenida/>} /> 
-              <Route path="auth" element={<VentanaLogin/>} /> 
-            </Route> 
->>>>>>> 89dd2e3cdb5e5ae54d807149a70d02891a8f2999
             {/* Para Dashboard Completo */}
             <Route path="/" element={<LayoutAdmin/>} >
               <Route index element={<Inicio/>}/>
@@ -57,50 +49,30 @@ function App() {
               <Route path="oficinadistrital" element={<OficinaDistritalPrincipal/>} />
 
               {/* TelefonoUrgencia */}
-<<<<<<< HEAD
               <Route path="telefonourgencia" element={<TelefonoUrgencia/>}/>
 
-=======
-              <Route path="telefonourgencia" element={<TelefonoUrgencia/>} />
->>>>>>> 89dd2e3cdb5e5ae54d807149a70d02891a8f2999
 
               {/* Rutas de Unidades Educativas */}
               <Route path="unidadeducativa" element={<ListaGeneralUE/>} />
 
-<<<<<<< HEAD
 
               {/* <Route path="unidadeducativa/agregarnuevo" element={<FormularioAgregarPublica/>} /> Formulario Antiguo*/}
               <Route path="unidadeducativa/agregarnuevo" element={<FormAgregarUE/>} />
              
-=======
-              {/* <Route path="unidadeducativa/agregarnuevo" element={<FormularioAgregarPublica/>} /> Formulario Antiguo*/}
-              <Route path="unidadeducativa/agregarnuevo" element={<FormAgregarUE/>} />
-             
-
->>>>>>> 89dd2e3cdb5e5ae54d807149a70d02891a8f2999
               <Route path="unidadeducativa/detalles/:id" element={<FormDataPublica/>} />
               <Route path="unidadeducativa/masdetalles/:id" element={<FormDataPublicaExtra/>} />
               <Route path="unidadeducativa/modificar/:id" element={<FormEditarUE/>} />
 
-<<<<<<< HEAD
               <Route path="centrosalud" element={<CentroSaludGeneral/>}/>
-=======
-              <Route path="centrosalud" element={<CentroSalud/>}/>
->>>>>>> 89dd2e3cdb5e5ae54d807149a70d02891a8f2999
               <Route path="centrorecreativo" element={<CentroRecreativo/>}/>
               
               {/* <Route index element={Vena}/> */}
 
-<<<<<<< HEAD
 
             </Route> 
 
               {/* <Route index element={<Principal/>} /> */}
 
-=======
-            </Route> 
-              {/* <Route index element={<Principal/>} /> */}
->>>>>>> 89dd2e3cdb5e5ae54d807149a70d02891a8f2999
           </Routes>
         </Router>
           
