a6702dd5691d764e88c0d6a59a06dfd723263d70
= <pumaronal2016@gmail.com>
2024-09-08 11:03:20 -0400

    Se agrego Componenten de pagina no encontrada

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const NotFound = () => {
   return (
     <div className=' flex justify-center items-center h-screen w-screen bg-gray-800'>
-      <div className='text-center'>
+      <div className='text-center p-5'>
         <h1 className='text-white font-extrabold text-4xl'>Error 404 - Página no encontrada</h1>
         <p className='text-white font-medium text-2xl'>La página que estás buscando no existe.</p>
       </div>
